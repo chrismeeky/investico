3e6818b4ae7437e8b91f7ac8f39ada9f6c63f8c1
chrismeeky <nwodochristian@gmail.com>
2019-10-31 11:03:24 +0100

    deploy base app

---
+++ src/config/mongoose.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const connectDatabase = async () => {
+  const connectionString = process.env.NODE_ENV === 'development'
+    ? process.env.DB_STRING_DEVELOPMENT : process.env.DB_STRING_TEST;
+  try {
+    const connected = await mongoose.connect(connectionString,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      });
+    if (connected) {
+      console.info('connection has been established');
+    }
+  } catch (error) {
+    console.info(error);
+  }
+};
+
+export default connectDatabase();
